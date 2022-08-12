1f7d1872871b78d9f9a8bb07669bf5379a6968a8
TopShorter <105965929+TopShorter@users.noreply.github.com>
2022-08-12 21:38:22 +0300

    Create neckable.sol

---
+++ neckable.sol
@@ -0,0 +1,22 @@
+                                                       |       |
+                                                        |     |
+                                                         |   |
+                                                          |||
+                                                           o
+                                                     ooooooooooooo
+                                                    oooooooooooooo*
+                                                   oooooooooooooooo*
+                                                ooooooooooooooooooo*
+                                                 ooo*oooooooooooooo*
+                                                 oo*ooooooooooooooo*
+                                                 o*oooo---oooo---oo*
+                                                  *ooooo---oooo---oo*
+                                                  **ooooooooooooooo*
+                                                   *ooooooooooooooo*
+                                                   *ooooooooooooooo*
+                                                   *oooooo_______oo*
+                                                   *oooooo-------oo*
+                                                   *ooooooooooooooo*
+                                                   *oooooooooooooo*
+                                                   *ooooooooo*****
+                                                   *ooo*
